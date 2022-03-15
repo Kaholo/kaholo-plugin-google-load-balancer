6edba33817c283fbf82b6921c3379e78f05525cd
michalkrzemieniewski <mkrzemien11@gmail.com>
2022-03-15 09:50:52 +0100

    README.md + refactor

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# kaholo-plugin-template
-A template repository for Kaholo plugins.
+# Kaholo GCP Load Balancer Plugin 
+GCP Load Balancer Plugin for Kaholo GCP
+This plugin uses Node.js client libraries for CCP Compute Engine:
+https://cloud.google.com/nodejs/docs/reference/compute/latest
+
+In order to get to know more about resources that you will able to create using this plugin visit:
+https://cloud.google.com/load-balancing/docs
+
+##  Settings
+1. Default Project ID (Vault) **Required if not in action** 
+2. Service Account Credentials (Vault) **Required if not in action** - GCP Credentials JSON
+3. Default Region (String) **Required if not in action** - The GCP region to make requests to on default.
+
+
+## Methods
+- Create HTTP External Load Balancer (With rollback [Read below](#loadbalancer))
+- Create HTTPS External Load Balancer (With rollback [Read below](#loadbalancer))
+
+
+- Create health check from JSON
+- Create backend service from JSON
+- Create URL map from JSON
+- Create target HTTP proxy from JSON 
+- Create target HTTPS proxy from JSON
+- Create forward rules from JSON
+- Create addresses from JSON 
+- Create backend bucket from JSON 
+- Create SSL certificate from JSON 
+- Create SSL policy from JSON 
+- Create target instance from JSON 
+- Create target pool from JSON  
+
+
+- Delete health check
+- Delete backend service
+- Delete URL map
+- Delete target HTTP proxy
+- Delete target HTTPS proxy
+- Delete forward rule
+- Delete address
+- Delete backend bucket
+- Delete SSL certificate
+- Delete SSL policy
+- Delete target instance
+- Delete target pool 
+
+
+##  <a name="loadbalancer"></a> Create HTTP and HTTPS External Load Balancer Methods
+
+This methods create preconfigured load balancer with minimal default configuration.
+
+The HTTP(S) method creates (in exact this order): 
+
+- Health check  
+- Backend service  
+- URL map  
+- Global target HTTP(S) proxy  
+- Global forwarding rule  
+
+
+**IMPORTANT**  
+If creation of one of the resources fails, then all resources that were created earlier are rollbacked.
--- google-compute-lib.js
+++ google-compute-lib.js
@@ -1,3 +1,5 @@
+/* eslint no-use-before-define: 0 */
+
 const gcpCompute = require("@google-cloud/compute");
 const _ = require("lodash");
 require("@google-cloud/compute");
@@ -12,52 +14,6 @@ const {
   getRegion,
   getZone,
 } = require("./helpers");
-
-function getAuthorizedClient(ClientClass, credentials) {
-  const clientInstance = new ClientClass({ credentials });
-  return { clientInstance, credentials };
-}
-
-async function callMethod(methodName, client, request, waitForOperation = false) {
-  if (waitForOperation) {
-    const operationsClient = new gcpCompute.GlobalOperationsClient({
-      credentials: client.credentials,
-    });
-
-    let [operation] = await client.clientInstance[methodName](request);
-
-    while (operation.status !== "DONE") {
-      // eslint-disable-next-line no-await-in-loop
-      [operation] = await operationsClient.wait({
-        operation: operation.name,
-        project: request.project,
-      });
-    }
-    return operation;
-  }
-  const result = await client.clientInstance[methodName](request);
-  return result;
-}
-
-async function callInsert(client, request, waitForOperation) {
-  return _.partial(callMethod, "insert")(client, request, waitForOperation);
-}
-
-async function callGet(client, request) {
-  return _.partial(callMethod, "get")(client, request);
-}
-
-async function callListAsync(client, request) {
-  return _.partial(callMethod, "listAsync")(client, request);
-}
-
-async function callDelete(client, request, waitForOperation = false) {
-  return _.partial(callMethod, "delete")(client, request, waitForOperation);
-}
-
-async function callSearchProjectsAsync(client, request) {
-  return _.partial(callMethod, "searchProjectsAsync")(client, request);
-}
 
 async function createResource(params, settings, clientClass, resource) {
   const credentials = getCredentials(params, settings);
@@ -99,6 +55,25 @@ async function deleteResourceWaitForDeletion(params, settings, clientClass, reso
   return deleteResource(paramsWithWaitForOperation, settings, clientClass, resource);
 }
 
+async function listGcpProjects(query, pluginSettings, pluginParams) {
+  const { result } = await getProjects(["projectId", "displayName"], pluginSettings, pluginParams);
+  return result;
+}
+
+async function listGcpRegions(query, pluginSettings, pluginParams) {
+  const { result } = await getListResults(["name"], pluginSettings, pluginParams, GCCompute.RegionsClient);
+  return result;
+}
+
+async function listGcpZones(query, pluginSettings, pluginParams) {
+  const { result, params } = await getListResults(["name"], pluginSettings, pluginParams, GCCompute.ZonesClient);
+
+  const region = parsers.autocomplete(params.region);
+  result.filter((zone) => !region || zone.name.includes(region));
+
+  return result;
+}
+
 async function getResource(params, settings, clientClass, resource) {
   const credentials = getCredentials(params, settings);
   const project = getProject(params, settings);
@@ -111,6 +86,55 @@ async function getResource(params, settings, clientClass, resource) {
     request,
   );
   return result[0];
+}
+
+async function getProjects(fields, pluginSettings, pluginParams) {
+  const parseFunc = getParseFromParam(...fields);
+  const settings = mapAutoParams(pluginSettings);
+  const params = mapAutoParams(pluginParams);
+
+  let result = await searchProjects(params, settings);
+  result = [...result.map(parseFunc)];
+  return { params, result };
+}
+
+async function searchProjects(params, settings) {
+  const credentials = getCredentials(params, settings);
+  const authorizedClient = getAuthorizedClient(ProjectsClient, credentials);
+  const { query } = params;
+
+  const request = { query: query ? `name:*${query}*` : undefined };
+
+  const iterable = await callSearchProjectsAsync(authorizedClient, request);
+  const res = [];
+
+  try {
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const proj of iterable) {
+      res.push(proj);
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
+
+  return res;
+}
+
+function createListItemsFunction(clientClass, fields) {
+  return async function listGcpItems(query, pluginSettings, pluginParams) {
+    const { result } = await getListResults(fields, pluginSettings, pluginParams, clientClass);
+    return result;
+  };
+}
+
+async function getListResults(fields, pluginSettings, pluginParams, clientClass) {
+  const parseFunc = getParseFromParam(...fields);
+  const settings = mapAutoParams(pluginSettings);
+  const params = mapAutoParams(pluginParams);
+
+  let result = await listResources(params, settings, clientClass);
+  result = [...result.map(parseFunc)];
+  return { params, result };
 }
 
 async function listResources(params, settings, clientClass, resource = {}) {
@@ -136,72 +160,50 @@ async function listResources(params, settings, clientClass, resource = {}) {
   return res;
 }
 
-async function searchProjects(params, settings) {
-  const credentials = getCredentials(params, settings);
-  const authorizedClient = getAuthorizedClient(ProjectsClient, credentials);
-  const { query } = params;
+async function callSearchProjectsAsync(client, request) {
+  return _.partial(callMethod, "searchProjectsAsync")(client, request);
+}
 
-  const request = { query: query ? `name:*${query}*` : undefined };
+async function callInsert(client, request, waitForOperation) {
+  return _.partial(callMethod, "insert")(client, request, waitForOperation);
+}
 
-  const iterable = await callSearchProjectsAsync(authorizedClient, request);
-  const res = [];
+async function callGet(client, request) {
+  return _.partial(callMethod, "get")(client, request);
+}
 
-  try {
-    // eslint-disable-next-line no-restricted-syntax
-    for await (const proj of iterable) {
-      res.push(proj);
+async function callListAsync(client, request) {
+  return _.partial(callMethod, "listAsync")(client, request);
+}
+
+async function callDelete(client, request, waitForOperation = false) {
+  return _.partial(callMethod, "delete")(client, request, waitForOperation);
+}
+
+async function callMethod(methodName, client, request, waitForOperation = false) {
+  if (waitForOperation) {
+    const operationsClient = new gcpCompute.GlobalOperationsClient({
+      credentials: client.credentials,
+    });
+
+    let [operation] = await client.clientInstance[methodName](request);
+
+    while (operation.status !== "DONE") {
+      // eslint-disable-next-line no-await-in-loop
+      [operation] = await operationsClient.wait({
+        operation: operation.name,
+        project: request.project,
+      });
     }
-  } catch (error) {
-    return Promise.reject(error);
+    return operation;
   }
-
-  return res;
-}
-
-async function getProjects(fields, pluginSettings, pluginParams) {
-  const parseFunc = getParseFromParam(...fields);
-  const settings = mapAutoParams(pluginSettings);
-  const params = mapAutoParams(pluginParams);
-
-  let result = await searchProjects(params, settings);
-  result = [...result.map(parseFunc)];
-  return { params, result };
-}
-
-async function getListResults(fields, pluginSettings, pluginParams, clientClass) {
-  const parseFunc = getParseFromParam(...fields);
-  const settings = mapAutoParams(pluginSettings);
-  const params = mapAutoParams(pluginParams);
-
-  let result = await listResources(params, settings, clientClass);
-  result = [...result.map(parseFunc)];
-  return { params, result };
-}
-
-function createListItemsFunction(clientClass, fields) {
-  return async function listGcpItems(query, pluginSettings, pluginParams) {
-    const { result } = await getListResults(fields, pluginSettings, pluginParams, clientClass);
-    return result;
-  };
-}
-
-async function listGcpProjects(query, pluginSettings, pluginParams) {
-  const { result } = await getProjects(["projectId", "displayName"], pluginSettings, pluginParams);
+  const result = await client.clientInstance[methodName](request);
   return result;
 }
 
-async function listGcpRegions(query, pluginSettings, pluginParams) {
-  const { result } = await getListResults(["name"], pluginSettings, pluginParams, GCCompute.RegionsClient);
-  return result;
-}
-
-async function listGcpZones(query, pluginSettings, pluginParams) {
-  const { result, params } = await getListResults(["name"], pluginSettings, pluginParams, GCCompute.ZonesClient);
-
-  const region = parsers.autocomplete(params.region);
-  result.filter((zone) => !region || zone.name.includes(region));
-
-  return result;
+function getAuthorizedClient(ClientClass, credentials) {
+  const clientInstance = new ClientClass({ credentials });
+  return { clientInstance, credentials };
 }
 
 module.exports = {

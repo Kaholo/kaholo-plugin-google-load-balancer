73d8320c6519fd00c66a8e88582af035de0e9110
Daniel Grad <daniel.g@kaholo.io>
2022-04-05 14:28:07 +0200

    Renamed gcp-compute-service-adapter to gcp-load-balancer

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {
 const {
   runHttpExternalLoadBalancerCreation,
   runHttpsExternalLoadBalancerCreation,
-} = require("./gcp-compute-service-adapter");
+} = require("./gcp-load-balancer");
 
 function generateGcpPluginMethod({
   GCPMethod,

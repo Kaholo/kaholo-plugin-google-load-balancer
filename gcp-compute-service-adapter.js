44318bf13433f012ebfaeef25bb54e1b4f052f6d
Daniel Grad <daniel.g@kaholo.io>
2022-04-05 14:21:24 +0200

    Refactored gcp-compute-service-adapter

--- gcp-compute-service-adapter.js
+++ gcp-compute-service-adapter.js
@@ -17,79 +17,100 @@ function createHealthCheckResource(action) {
 }
 
 async function createBackendServiceResource(action, credentials, project) {
+  const instanceGroupsResource = await callResourceOperation(
+    RESOURCE_OPERATIONS.get,
+    GCCompute.InstanceGroupsClient,
+    credentials,
+    project,
+    {
+      zone: action.params.zone.value,
+      instanceGroup: action.params.instanceGroupName.value,
+    },
+  );
+
+  const healthCheckResource = await callResourceOperation(
+    RESOURCE_OPERATIONS.get,
+    GCCompute.HealthChecksClient,
+    credentials,
+    project,
+    {
+      zone: action.params.zone.value,
+      healthCheck: action.params.healthCheckName,
+    },
+  );
+
   const backendServiceResource = {
     backendServiceResource: {
       name: action.params.backendServiceName,
       backends: [
         {
-          group: (await callResourceOperation(
-            RESOURCE_OPERATIONS.get,
-            GCCompute.InstanceGroupsClient,
-            credentials,
-            project,
-            {
-              zone: action.params.zone.value,
-              instanceGroup: action.params.instanceGroupName.value,
-            },
-          )).selfLink,
+          group: instanceGroupsResource.selfLink,
         },
       ],
       healthChecks: [
-        (await callResourceOperation(
-          RESOURCE_OPERATIONS.get,
-          GCCompute.HealthChecksClient,
-          credentials,
-          project,
-          {
-            zone: action.params.zone.value,
-            healthCheck: action.params.healthCheckName,
-          },
-        )).selfLink,
+        healthCheckResource.selfLink,
       ],
     },
   };
+
   return backendServiceResource;
 }
 
 async function createUrlMapResource(action, credentials, project) {
+  const defaultServiceResource = await callResourceOperation(
+    RESOURCE_OPERATIONS.get,
+    GCCompute.BackendServicesClient,
+    credentials,
+    project,
+    {
+      backendService: action.params.backendServiceName,
+    },
+  );
+
   const urlMapResource = {
     urlMapResource: {
       name: action.params.urlMapName,
-      defaultService: (await callResourceOperation(
-        RESOURCE_OPERATIONS.get,
-        GCCompute.BackendServicesClient,
-        credentials,
-        project,
-        {
-          backendService: action.params.backendServiceName,
-        },
-      )).selfLink,
+      defaultService: defaultServiceResource.selfLink,
     },
   };
+
   return urlMapResource;
 }
 
 async function createTargetHttpProxyResource(action, credentials, project) {
+  const urlMapsResource = await callResourceOperation(
+    RESOURCE_OPERATIONS.get,
+    GCCompute.UrlMapsClient,
+    credentials,
+    project,
+    {
+      urlMap: action.params.urlMapName,
+    },
+  );
+
   const targetHttpProxyResource = {
     targetHttpProxyResource:
       {
         name: action.params.httpProxyName,
-        urlMap: (await callResourceOperation(
-          RESOURCE_OPERATIONS.get,
-          GCCompute.UrlMapsClient,
-          credentials,
-          project,
-          {
-            urlMap: action.params.urlMapName,
-          },
-        )).selfLink,
+        urlMap: urlMapsResource.selfLink,
       },
   };
+
   return targetHttpProxyResource;
 }
 
 async function createTargetHttpsProxyResource(action, credentials, project) {
-  const sslCertificateURL = (await callResourceOperation(
+  const urlMapsResource = await callResourceOperation(
+    RESOURCE_OPERATIONS.get,
+    GCCompute.UrlMapsClient,
+    credentials,
+    project,
+    {
+      urlMap: action.params.urlMapName,
+    },
+  );
+
+  const sslCertificateURL = await callResourceOperation(
     RESOURCE_OPERATIONS.get,
     GCCompute.SslCertificatesClient,
     credentials,
@@ -97,51 +118,53 @@ async function createTargetHttpsProxyResource(action, credentials, project) {
     {
       sslCertificate: action.params.sslCertificateName.value,
     },
-  )).selfLink;
+  );
 
   const targetHttpsProxyResource = {
     targetHttpsProxyResource:
       {
         name: action.params.httpsProxyName,
-        urlMap: (await callResourceOperation(
-          RESOURCE_OPERATIONS.get,
-          GCCompute.UrlMapsClient,
-          credentials,
-          project,
-          {
-            urlMap: action.params.urlMapName,
-          },
-        )).selfLink,
-        sslCertificates: [sslCertificateURL],
+        urlMap: urlMapsResource.selfLink,
+        sslCertificates: [sslCertificateURL.selfLink],
       },
   };
   return targetHttpsProxyResource;
 }
 
-async function createForwardingRuleResource(action, credentials, project) {
-  let target;
-  if (action.params.httpProxyName) {
-    target = (await callResourceOperation(
-      RESOURCE_OPERATIONS.get,
-      GCCompute.TargetHttpProxiesClient,
-      credentials,
-      project,
-      { targetHttpProxy: action.params.httpProxyName },
-    )).selfLink;
-  } else {
-    target = (await callResourceOperation(
-      RESOURCE_OPERATIONS.get,
-      GCCompute.TargetHttpsProxiesClient,
-      credentials,
-      project,
-      { targetHttpsProxy: action.params.httpsProxyName },
-    )).selfLink;
-  }
+async function createHttpForwardingRuleResource(action, credentials, project) {
+  const targetHttpProxyClient = await callResourceOperation(
+    RESOURCE_OPERATIONS.get,
+    GCCompute.TargetHttpProxiesClient,
+    credentials,
+    project,
+    { targetHttpProxy: action.params.httpProxyName },
+  );
 
   const forwardingRuleResource = {
     forwardingRuleResource: {
       name: action.params.forwardingRuleName,
-      target,
+      target: targetHttpProxyClient.selfLink,
+      portRange: action.params.forwardRulePortRange,
+      loadBalancingScheme: "EXTERNAL",
+    },
+  };
+
+  return forwardingRuleResource;
+}
+
+async function createHttpsForwardingRuleResource(action, credentials, project) {
+  const targetHttpsProxyClient = await callResourceOperation(
+    RESOURCE_OPERATIONS.get,
+    GCCompute.TargetHttpsProxiesClient,
+    credentials,
+    project,
+    { targetHttpsProxy: action.params.httpsProxyName },
+  );
+
+  const forwardingRuleResource = {
+    forwardingRuleResource: {
+      name: action.params.forwardingRuleName,
+      target: targetHttpsProxyClient.selfLink,
       portRange: action.params.forwardRulePortRange,
       loadBalancingScheme: "EXTERNAL",
     },
@@ -149,27 +172,31 @@ async function createForwardingRuleResource(action, credentials, project) {
   return forwardingRuleResource;
 }
 
-function createLoadBalancerResourcesData(proxyType) {
-  return [
-    { client: GCCompute.HealthChecksClient, createResourceFunc: createHealthCheckResource, typeProperty: "healthCheck" },
-    { client: GCCompute.BackendServicesClient, createResourceFunc: createBackendServiceResource, typeProperty: "backendService" },
-    { client: GCCompute.UrlMapsClient, createResourceFunc: createUrlMapResource, typeProperty: "urlMap" },
-    proxyType === "http"
-      ? { client: GCCompute.TargetHttpProxiesClient, createResourceFunc: createTargetHttpProxyResource, typeProperty: "targetHttpProxy" }
-      : { client: GCCompute.TargetHttpsProxiesClient, createResourceFunc: createTargetHttpsProxyResource, typeProperty: "targetHttpsProxy" },
-    { client: GCCompute.GlobalForwardingRulesClient, createResourceFunc: createForwardingRuleResource, typeProperty: "forwardingRule" },
-  ];
-}
+const httpLoadBalancerServicesDefinitions = [
+  { client: GCCompute.HealthChecksClient, createResourceFunc: createHealthCheckResource, typeProperty: "healthCheck" },
+  { client: GCCompute.BackendServicesClient, createResourceFunc: createBackendServiceResource, typeProperty: "backendService" },
+  { client: GCCompute.UrlMapsClient, createResourceFunc: createUrlMapResource, typeProperty: "urlMap" },
+  { client: GCCompute.TargetHttpProxiesClient, createResourceFunc: createTargetHttpProxyResource, typeProperty: "targetHttpProxy" },
+  { client: GCCompute.GlobalForwardingRulesClient, createResourceFunc: createHttpForwardingRuleResource, typeProperty: "forwardingRule" },
+];
+
+const httpsLoadBalancerServicesDefinitions = [
+  { client: GCCompute.HealthChecksClient, createResourceFunc: createHealthCheckResource, typeProperty: "healthCheck" },
+  { client: GCCompute.BackendServicesClient, createResourceFunc: createBackendServiceResource, typeProperty: "backendService" },
+  { client: GCCompute.UrlMapsClient, createResourceFunc: createUrlMapResource, typeProperty: "urlMap" },
+  { client: GCCompute.TargetHttpsProxiesClient, createResourceFunc: createTargetHttpsProxyResource, typeProperty: "targetHttpsProxy" },
+  { client: GCCompute.GlobalForwardingRulesClient, createResourceFunc: createHttpsForwardingRuleResource, typeProperty: "forwardingRule" },
+];
 
 module.exports = {
   runHttpExternalLoadBalancerCreation: (action, settings) => createMultipleGCPServices(
-    createLoadBalancerResourcesData("http"),
+    httpLoadBalancerServicesDefinitions,
     action,
     helpers.getCredentials(action.params, settings),
     helpers.getProject(action.params, settings),
   ),
   runHttpsExternalLoadBalancerCreation: (action, settings) => createMultipleGCPServices(
-    createLoadBalancerResourcesData("https"),
+    httpsLoadBalancerServicesDefinitions,
     action,
     helpers.getCredentials(action.params, settings),
     helpers.getProject(action.params, settings),
